8bf74cef130d44f2e6a4aadce453ec322ff5e1b2
AntonChetveryk <chetverykanton92@gmail.com>
2020-09-17 16:03:14 +0300

    Updates

---
+++ precache-manifest.db5de347878a4ebd3c07a58f3f42040f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e1ee0e8f74982df50ab37accc2b8cf",
+    "url": "/nasa/index.html"
+  },
+  {
+    "revision": "033deb7689ee17ada444",
+    "url": "/nasa/static/css/main.e5336dc9.chunk.css"
+  },
+  {
+    "revision": "0e153aa3126f6877e511",
+    "url": "/nasa/static/js/2.bcdd698c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nasa/static/js/2.bcdd698c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033deb7689ee17ada444",
+    "url": "/nasa/static/js/main.6cd60963.chunk.js"
+  },
+  {
+    "revision": "649df1e927f3ecb3dd5c",
+    "url": "/nasa/static/js/runtime-main.30ff7636.js"
+  }
+]);
